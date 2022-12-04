03e272f04c43329849524df4aac9846b400f4753
Daisy Le <daisy.le0203@gmail.com>
2022-12-04 17:40:17 -0600

    turn type from string to constants to avoid error

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -1,3 +1,4 @@
+import { FETCH_ALL, CREATE, UPDATE, DELETE, LIKE } from "../constants/actionTypes"
 // Import everything from actions as api
 // We now are be able to use fetchPosts and createPost
 import * as api from "../api"
@@ -12,7 +13,7 @@ export const getPosts = () => async (dispatch) => {
     // Data represents the posts
     const { data } = await api.fetchPosts()
 
-    dispatch({ type: "FETCH_ALL", payload: data })
+    dispatch({ type: FETCH_ALL, payload: data })
   } catch (error) {
     console.log(error)
   }
@@ -23,7 +24,7 @@ export const createPost = (post) => async (dispatch) => {
     // Destructing data from the response and making POST api request to the backend server and sending post
     const { data } = await api.createPost(post)
 
-    dispatch({ type: "CREATE", payload: data })
+    dispatch({ type: CREATE, payload: data })
   } catch (error) {
     console.log(error)
   }
@@ -33,7 +34,7 @@ export const updatePost = (id, post) => async (dispatch) => {
   try {
     const { data } = await api.updatePost(id, post)
 
-    dispatch({ type: "UPDATE", payload: data })
+    dispatch({ type: UPDATE, payload: data })
   } catch (error) {
     console.log(error)
   }
@@ -43,7 +44,7 @@ export const deletePost = (id) => async (dispatch) => {
   try {
     await api.deletePost(id)
 
-    dispatch({ type: "DELETE", payload: id })
+    dispatch({ type: DELETE, payload: id })
   } catch (error) {
     console.log(error)
   }
@@ -53,7 +54,7 @@ export const likePost = (id) => async (dispatch) => {
   try {
     const { data } = await api.likePost(id)
   
-    dispatch({ type: "LIKE", payload: data })
+    dispatch({ type: LIKE, payload: data })
   } catch (error) {
     console.log(error);
   }
--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,19 +1,21 @@
+import { FETCH_ALL, CREATE, UPDATE, DELETE, LIKE } from "../constants/actionTypes"
+
 // Reducer is a function that accepts a state which is posts and accept action
 // Based on the action type then we can update the state
 export default (posts = [], action) => {
   // action.type is the key
   switch (action.type) {
     // Specify all the types/action
-    case "DELETE":
+    case DELETE:
       return posts.filter((post) => post._id !== action.payload)
-    case "UPDATE":
-    case "LIKE":
+    case UPDATE:
+    case LIKE:
       return posts.map((post) =>
         post._id === action.payload._id ? action.payload : post
       )
-    case "FETCH_ALL":
+    case FETCH_ALL:
       return action.payload // this is the actual posts
-    case "CREATE":
+    case CREATE:
       return [...posts, action.payload] // spread all the post and add the new post
     default:
       return posts
