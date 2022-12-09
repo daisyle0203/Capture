d33e670a24cd38aef5f82b4177bcf7129942e3bc
Daisy Le <daisy.le0203@gmail.com>
2022-12-09 15:38:22 -0600

    install concurrently to connect client and server

--- server/index.js
+++ server/index.js
@@ -3,7 +3,11 @@ import express from "express"
 import mongoose from "mongoose"
 import cors from "cors"
 import dotenv from "dotenv"
-
+import path from "path"
+import {fileURLToPath} from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+console.log(__dirname)
 // Import the routes
 import postRoutes from "./routes/posts.js"
 import userRoutes from "./routes/users.js"
@@ -13,7 +17,6 @@ const app = express()
 
 // Call dotenv
 dotenv.config()
-
 // Set the limit to 30mb because we will send images which can be large in size
 app.use(express.json({ limit: "30mb" }));
 app.use(express.urlencoded({ extended: true, limit: "30mb" }));
@@ -26,8 +29,16 @@ app.use(cors())
 app.use("/posts", postRoutes)
 app.use("/user", userRoutes)
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
 // Set up port
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5001
 
 // Setup connection to mongodb.com
 mongoose
